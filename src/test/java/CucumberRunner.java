7471204eec09a774da3046e482afb6225df15ce8
ARushan@appline.ru <ARushan@appline.ru>
2023-03-17 11:09:06 +0300

    Формируется онлайн отчет, не полный. cucumber7

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        strict = true,
+        publish = true,
         features = {"src/test/features"},
         glue = {"steps", "hooks"},
         tags = "@all",
